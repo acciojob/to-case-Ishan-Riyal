764d25616bead833c9c01b3e0d744f83193c2f62
Ishan Riyal <ishanriyal07@gmail.com>
2025-05-31 19:22:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function toCase(text) {
   // write your code here
+	const upper = text.toUpperCase();
+	const lower = text.toLowerCase();
+	return `${lower}-${upper}`
 }
 
 // DO not change the code below
 
-const text = prompt("Enter text:");
-alert(toCase(text));
+// const text = prompt("Enter text:");
+// alert(toCase(text));
